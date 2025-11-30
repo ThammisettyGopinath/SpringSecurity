750c7877196a57fb2a7b4e2c984df35e66f90416
MY_PC <thammisettygopinath143@gmail.com>
2025-11-30 14:35:07 +0530

    spring security

---
+++ spring-sec/src/main/java/com/security/app/model/UserPrincipal.java
@@ -0,0 +1,40 @@
+package com.security.app.model;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserPrincipal implements UserDetails{
+
+	private Users users;
+	
+	
+	
+	public UserPrincipal(Users user) {
+		this.users=user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return Collections.singleton(new SimpleGrantedAuthority("USER"));
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return users.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return users.getName();
+	}
+	
+	
+
+}
